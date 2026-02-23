167212ee67cca55c7f2855e133b25dd3f83ba35e
T. Todua <7117978+ttodua@users.noreply.github.com>
2026-02-23 10:13:52 +0400

    perf: python keysort (80%) (#27781)
    perf: python keysort 70% speedup

--- python/ccxt/base/exchange.py
+++ python/ccxt/base/exchange.py
@@ -987,7 +987,7 @@ class Exchange(object):
 
     @staticmethod
     def keysort(dictionary):
-        return collections.OrderedDict(sorted(dictionary.items(), key=lambda t: t[0]))
+        return dict(sorted(dictionary.items()))
 
     @staticmethod
     def sort(array):
