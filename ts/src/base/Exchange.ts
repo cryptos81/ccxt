09f4d2fa6e1c549ac2cc31f0800c31d703315a02
T. Todua <7117978+ttodua@users.noreply.github.com>
2026-05-04 17:26:13 +0400

    `costToPrecision` - fix (#14269)
    * some exchanges have cost defined
    
    * Update Exchange.ts

--- ts/src/base/Exchange.ts
+++ ts/src/base/Exchange.ts
@@ -7338,7 +7338,7 @@ export default class Exchange {
             return undefined;
         }
         const market = this.market (symbol);
-        return this.decimalToPrecision (cost, TRUNCATE, market['precision']['price'], this.precisionMode, this.paddingMode);
+        return this.decimalToPrecision (cost, TRUNCATE, this.safeString2 (market['precision'], 'cost', 'price'), this.precisionMode, this.paddingMode);
     }
 
     priceToPrecision (symbol: string, price): string {
